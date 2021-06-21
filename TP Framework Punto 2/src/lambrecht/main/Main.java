214bd306c8bcd9316a93f3f66e791edc2d5e0233
54292 <54292@192.168.1.116>
2021-06-21 15:07:55 -0300

    TP Framework Punto 2

---
+++ TP Framework Punto 2/src/lambrecht/main/Main.java
@@ -0,0 +1,12 @@
+package lambrecht.main;
+
+import lambrecht.framework.PantallaAciones;
+
+public class Main {
+
+	public static void main(String[] args) {
+		PantallaAciones acciones = new PantallaAciones();
+		acciones.desplegarMenu();
+	}
+
+}
